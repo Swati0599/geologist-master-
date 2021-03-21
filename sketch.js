3c9486384fba8f4f28c3dddbcc27aab362588123
Swati0599 <65229725+Swati0599@users.noreply.github.com>
2021-03-21 09:51:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+   
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+   
+}
